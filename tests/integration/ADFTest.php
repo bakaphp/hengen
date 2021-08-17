3072012cb4b7b746c68686093753d577c62e6a1a
FredPeal <frederickpeal@gmail.com>
2021-08-17 01:25:24 -0400

    refactor!: create data before test

--- tests/integration/ADFTest.php
+++ tests/integration/ADFTest.php
@@ -2,10 +2,14 @@
 
 use Canvas\Models\Companies;
 
+use Faker\Factory;
+use Faker\Provider\en_US\Person;
+use Faker\Provider\en_US\PhoneNumber;
+use Faker\Provider\Internet;
+
 use Hengen\Hengen;
 use Hengen\Tests\Support\Models\Leads;
 use Hengen\Tests\Support\Models\Vehicles;
-
 use Hengen\Tests\Support\Models\Vendors;
 
 class ADFTest extends \Codeception\Test\Unit
@@ -17,6 +21,31 @@ class ADFTest extends \Codeception\Test\Unit
 
     protected function _before()
     {
+        $faker = Factory::create();
+        $faker->addProvider(new Person($faker));
+        $faker->addProvider(new PhoneNumber($faker));
+        $faker->addProvider(new Internet($faker));
+
+        $data = [
+            'firstname' => $faker->name,
+            'lastname' => $faker->address,
+            'email' => $faker->email,
+            'phone' => $faker->phoneNumber
+        ];
+
+        $dataVehicle = [
+            'vin' => rand(10000, 100000),
+            'make' => 'American Vehicle',
+            'model' => 'V1',
+            'int_color' => 'grey',
+            'ext_color' => 'blue'
+        ];
+
+        $leads = new Leads();
+        $leads->saveOrFail($data);
+
+        $vehicles = new Vehicles();
+        $vehicles->saveOrFail($dataVehicle);
     }
 
     protected function _after()
@@ -26,10 +55,10 @@ class ADFTest extends \Codeception\Test\Unit
     // tests
     public function testHengen()
     {
-        $lead = Leads::findFirst();
-        $vendors = Vendors::findFirst();
-        $vehicles = Vehicles::findFirst();
-        $companies = Companies::findFirst();
+        $lead = Leads::findFirstOrFail();
+        $vendors = Vendors::findFirstOrFail();
+        $vehicles = Vehicles::findFirstOrFail();
+        $companies = Companies::findFirstOrFail();
 
         $transformer = Hengen::getTransformer('ADF', $lead, ['template' => 'dealer-content'], $vendors, $vehicles);
         $communicator = Hengen::getCommunication($transformer, $companies);
